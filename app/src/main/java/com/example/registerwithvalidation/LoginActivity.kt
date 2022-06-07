68056894a6947da1bb7f5a667600f9ab58e60c8c
jeongmin <tmxk0810@gmail.com>
2022-06-07 18:05:06 +0900

    기존의 파이어베이스를 이용해 가입하는 예제에 이메일이 중복될 경우, 비밀번호가 8글자 미만일 경우, 이메일, 비밀번호 칸 중 하나라도 공백일 경우의 처리를 모두 한 예제

---
+++ app/src/main/java/com/example/registerwithvalidation/LoginActivity.kt
@@ -0,0 +1,138 @@
+package com.example.registerwithvalidation
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Toast
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.GoogleAuthProvider
+import kotlinx.android.synthetic.main.activity_login.*
+
+class LoginActivity : AppCompatActivity(), View.OnClickListener {
+
+    //firebase Auth
+    private lateinit var firebaseAuth: FirebaseAuth
+    //google client
+    private lateinit var googleSignInClient: GoogleSignInClient
+
+    //private const val TAG = "GoogleActivity"
+    private val RC_SIGN_IN = 99
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        //btn_googleSignIn.setOnClickListener (this) // 구글 로그인 버튼
+        btnGoogleSignIn.setOnClickListener {signIn()}
+
+        //Google 로그인 옵션 구성. requestIdToken 및 Email 요청
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken("716839901747-a8m6mv3r49rj2rbulk42bkd1qnmv7iqi.apps.googleusercontent.com")
+            .requestEmail()
+            .build()
+
+        googleSignInClient = GoogleSignIn.getClient(this, gso)
+
+        //firebase auth 객체
+        firebaseAuth = FirebaseAuth.getInstance()
+    }
+
+    // onStart. 유저가 앱에 이미 구글 로그인을 했는지 확인
+    public override fun onStart() {
+        super.onStart()
+        val account = GoogleSignIn.getLastSignedInAccount(this)
+        if(account!==null){ // 이미 로그인 되어있을시 바로 메인 액티비티로 이동
+
+            toMainActivity(firebaseAuth.currentUser)
+        }
+    } //onStart End
+
+    // onActivityResult
+    public override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            try {
+                // Google Sign In was successful, authenticate with Firebase
+                val account = task.getResult(ApiException::class.java)
+                firebaseAuthWithGoogle(account!!)
+
+            } catch (e: ApiException) {
+                // Google Sign In failed, update UI appropriately
+                Log.w("LoginActivity", "Google sign in failed", e)
+            }
+        }
+    } // onActivityResult End
+
+    // firebaseAuthWithGoogle
+    private fun firebaseAuthWithGoogle(acct: GoogleSignInAccount) {
+        Log.d("LoginActivity", "firebaseAuthWithGoogle:" + acct.id!!)
+
+        //Google SignInAccount 객체에서 ID 토큰을 가져와서 Firebase Auth로 교환하고 Firebase에 인증
+        val credential = GoogleAuthProvider.getCredential(acct.idToken, null)
+        firebaseAuth.signInWithCredential(credential)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    Log.w("LoginActivity", "firebaseAuthWithGoogle 성공", task.exception)
+                    toMainActivity(firebaseAuth?.currentUser)
+                } else {
+                    Log.w("LoginActivity", "firebaseAuthWithGoogle 실패", task.exception)
+//                    Snackbar.make(login_layout, "로그인에 실패하였습니다.", Snackbar.LENGTH_SHORT).show()
+                }
+            }
+    }// firebaseAuthWithGoogle END
+
+
+    // toMainActivity
+    fun toMainActivity(user: FirebaseUser?) {
+        if(user !=null) { // MainActivity 로 이동
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }
+    } // toMainActivity End
+
+    // signIn
+    private fun signIn() {
+        val signInIntent = googleSignInClient.signInIntent
+        startActivityForResult(signInIntent, RC_SIGN_IN)
+    }
+    // signIn End
+
+    override fun onClick(p0: View?) {
+    }
+
+    private fun signIn(email : String, password : String) {
+        if(email.isNotEmpty() && password.isNotEmpty()) {
+            firebaseAuth?.signInWithEmailAndPassword(email, password)
+                ?.addOnCompleteListener(this) {task ->
+                    if(task.isSuccessful) {
+                        Toast.makeText(this, "로그인에 성공했습니다.", Toast.LENGTH_SHORT).show()
+                        toMainActivity(firebaseAuth?.currentUser)
+                    } else {
+                        Toast.makeText(this, "로그인에 실패했습니다.", Toast.LENGTH_SHORT).show()
+                    }
+                }
+        } else {
+            Toast.makeText(this, "이메일과 비밀번호 입력칸을 모두 채워주세요.", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    fun onClickLogin(view: View) {
+        signIn(etEnterEmail.text.toString(), etEnterPassword.text.toString())
+    }
+
+    fun onClickRegister(view: View) {
+        val intent = Intent(this, RegisterActivity::class.java)
+        startActivity(intent)
+    }
+}
---
+++ app/src/main/java/com/example/registerwithvalidation/RegisterActivity.kt
@@ -0,0 +1,79 @@
+package com.example.registerwithvalidation
+
+import android.content.Intent
+import android.graphics.Color
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import kotlinx.android.synthetic.main.activity_register.*
+
+class RegisterActivity : AppCompatActivity() {
+
+    private lateinit var auth : FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+
+        auth = FirebaseAuth.getInstance()
+    }
+    private fun createUser(email : String, password : String) {
+        auth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener { task ->
+                if(task.isSuccessful) {
+                    Toast.makeText(this, "회원가입 성공\n다시 로그인해주세요.", Toast.LENGTH_SHORT).show()
+                    val user = auth.currentUser
+                    updateUI(user)
+                    val intent = Intent(this, LoginActivity::class.java)
+                    startActivity(intent)
+
+                } else { // 형식에 맞지 않을 경우
+
+                    if(password.toString().length < 8) {
+                        tvValidationPassword.text = "비밀번호를 8글자 이상으로 설정 해 주세요.\n"
+                        tvValidationPassword.setTextColor(Color.RED)
+                    } else if(password.toString().length >= 8) {
+                        tvValidationPassword.text = "8글자 이상의 안전한 비밀번호입니다."
+                        tvValidationPassword.setTextColor(Color.BLUE)
+                    }
+                    if(!android.util.Patterns.EMAIL_ADDRESS.matcher(etEmail.text).matches()) {
+                        tvValidationEmail.text = "올바른 이메일 형식이 아닙니다.\n"
+                        tvValidationEmail.setTextColor(Color.RED)
+                    } else if(android.util.Patterns.EMAIL_ADDRESS.matcher(etEmail.text).matches()) {
+                        tvValidationEmail.text = "올바른 이메일 형식입니다.\n"
+                        tvValidationEmail.setTextColor(Color.BLUE)
+                    }
+                    updateUI(null)
+                }
+            }
+            .addOnFailureListener { // 파이어베이스의 데이터와 중복됐을 경우
+                Toast.makeText(this, "이미 가입된 이메일입니다.\n다른 이메일을 입력해주세요.", Toast.LENGTH_SHORT).show()
+                tvValidationEmail.text = "이메일 형식은 맞지만,\n 이미 가입되어 있는 이메일입니다."
+                tvValidationEmail.setTextColor(Color.RED)
+                etEmail.text.clear()
+            }
+        return
+    }
+
+    private fun updateUI(user : FirebaseUser?) {
+        user?.let{
+            tvResult.text = "Email : ${user.email}\nUid : ${user.uid}"
+        }
+    }
+
+    fun onClickRegistration(view: View) {
+        val email = etEmail.text.toString().trim()
+        val password = etPassword.text.toString().trim()
+
+
+        if(email == "" || password == "") { // 공백에러
+            Toast.makeText(this, "이메일, 비밀번호는 반드시 입력해야합니다.", Toast.LENGTH_SHORT).show()
+
+        } else { // 공백이 아닐 경우 계정 생성
+            createUser(email, password)
+        }
+    }
+}
